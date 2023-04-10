6bf467ce99ff7b00b0d4e51ad8aed735ca20a7d4
wbwb014 <118424724+wbwb014@users.noreply.github.com>
2023-04-11 01:18:50 +0700

    push light

--- AnimationFunction.cs
+++ AnimationFunction.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class AnimationFunction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField]MenuButtonController menuButtonController;
+    // Start is called before the first frame update
+    
+    void PlaySound(AudioClip whichSound){
+        menuButtonController.audioSource.PlayOneShot(whichSound);
     }
 }
--- Lighting.cs
+++ Lighting.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuButtonController : MonoBehaviour
+public class Lighting : MonoBehaviour
 {
-    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
-    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
