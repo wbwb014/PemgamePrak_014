07bc565a00c0109ec72fa9cd53b3cf20fb0ae236
wbwb014 <118424724+wbwb014@users.noreply.github.com>
2023-05-05 00:00:06 +0700

    CamShakeFix

--- CameraShake.cs
+++ CameraShake.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
+    [SerializeField] private float power = 0.05f;
+    [SerializeField] private float duration = 0.25f;
+    private float slowDownAmount = 1f;
+    private bool shouldShake = false;
+    public Transform camera;
+    Vector3 startPosition;
+    float initialDuration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.Find("Camera").transform;
+        startPosition = camera.localPosition;
+        initialDuration = duration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(shouldShake){
+            if(duration > 0){
+                camera.localPosition = startPosition + Random.insideUnitSphere * power;
+                duration -= Time.deltaTime * slowDownAmount;
+            }else{
+            shouldShake = false;
+            duration = initialDuration;
+            camera.localPosition = startPosition;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Obstacle"){
+            shouldShake = true;
+        }
     }
 }
